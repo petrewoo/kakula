8a9f5a2566cb108acd7c8202581b3544c9968d7e
Alex <petrewoo@hotmail.com>
2018-03-15 18:08:15 +0800

    Tutorial part4 without generic view.

--- kakula/polls/views.py
+++ kakula/polls/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
-# Create your views here.
-
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -17,9 +16,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "path('<int:question_id>/', views.detail, name='detail'),"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
